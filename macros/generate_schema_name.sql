380ef94bf5f62a3201d0a641394b5821ebebf4a2
Jessica Stayton <jessica.stayton+old@dbtlabs.com>
2025-12-12 16:24:50 +0000

    test

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,4 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+-- rely on target to tell dbt which schema to use, custom or default
+-- set target in each developer profile to 'dev' 
+-- set target in each job to 'prod' or 'staging' 
 
     {% set default_schema = target.schema %}
 
