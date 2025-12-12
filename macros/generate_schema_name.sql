44aa2c52838753eec1d495be8640d0f82544fd8f
Jessica Stayton <jessica.stayton+old@dbtlabs.com>
2025-12-12 16:18:53 +0000

    test

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,4 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+    
+    {% set use_custom_schema = env_var('DBT_USE_CUSTOM_SCHEMA', 'false') | lower %}
 
     {% set default_schema = target.schema %}
 
@@ -6,17 +8,13 @@
 
         {{ default_schema }}
 
-    {% elif target.name in ['prod', 'staging'] %}
+    {% elif use_custom_schema == 'true' %}
 
         {{ custom_schema_name | trim }}
-
-    {% elif target.name in ['default', 'dev'] %}
-
-        {{ default_schema }}
 
     {% else %}
 
-        {{ custom_schema_name | trim }}
+        {{ default_schema }}
 
     {% endif %}
 
