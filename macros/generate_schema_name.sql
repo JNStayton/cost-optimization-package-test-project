9de71b1917298783810f9e129398151fa05c75da
Jessica Stayton <jessica.stayton+old@dbtlabs.com>
2025-12-12 16:31:56 +0000

    test

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,6 +1,4 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
--- rely on dbt env variable to let us know which schema to use (custom or default)
--- set ONE env var DBT_USE_CUSTOM_SCHEMA with true or false for each env
     
     {% set use_custom_schema = env_var('DBT_USE_CUSTOM_SCHEMA', 'false') | lower %}
 
