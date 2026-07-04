e166141011c9379b70562813214c1a053caf4e59
Jessica Stayton <jessica.stayton+old@dbtlabs.com>
2026-07-04 17:54:40 +0000

    Testing CI

--- models/marts/fct_revenue_by_customer_nation.sql
+++ models/marts/fct_revenue_by_customer_nation.sql
@@ -39,9 +39,8 @@ select
     order_items.order_key,
     order_items.order_date,
     customers.customer_key,
-    customers.nation,
-    customers.nation_key
-
+    customers.nation_key,
+    customers.nation
 
 from
     order_items
